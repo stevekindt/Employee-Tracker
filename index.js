756b65f224aca24cf0190bddb0753696aafe4ee9
Steve Kindt <stevemkindt@gmail.com>
2020-03-13 22:10:55 -0500

    main menu prompt

--- index.js
+++ index.js
@@ -15,4 +15,52 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  mainMenu();
 });
+
+// Main inquirer menu
+function mainMenu() {
+  inquirer
+    .prompt({
+      type: "list",
+      message: "What would you ike to do?",
+      name: "choices",
+      choices: [
+        {
+          name: "View all employees",
+          value: "viewEmployees"
+        },
+        {
+          name: "View all departments",
+          value: "viewDepartments"
+        },
+        {
+          name: "View all roles",
+          value: "viewRoles"
+        },
+        {
+          name: "Add employee",
+          value: "addEmployee"
+        },
+        {
+          name: "Add department",
+          value: "addDepartment"
+        },
+        {
+          name: "Add role",
+          value: "addRole"
+        },
+        {
+          name: "Update role",
+          value: "updateRole"
+        },
+        {
+          name: "Quit",
+          value: "quit"
+        }
+      ]
+    })
+    .then(function(res) {
+      menu(res.choices);
+    });
+}

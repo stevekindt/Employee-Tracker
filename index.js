1c6caa2e6f64b45b73b8f4657991915a02596e14
Steve Kindt <stevemkindt@gmail.com>
2020-03-14 15:39:24 -0500

    view functions

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Dependencies
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require("console.table");
 
 // Database connection
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -23,7 +22,7 @@ function mainMenu() {
   inquirer
     .prompt({
       type: "list",
-      message: "What would you ike to do?",
+      message: "What would you like to do?",
       name: "choices",
       choices: [
         {
@@ -63,4 +62,65 @@ function mainMenu() {
     .then(function(res) {
       menu(res.choices);
     });
+}
+
+function menu(option) {
+  switch (option) {
+    case "viewEmployees":
+      viewEmployees();
+      break;
+    case "viewDepartments":
+      viewDepartments();
+      break;
+    case "viewRoles":
+      viewRoles();
+      break;
+    case "addEmployee":
+      addEmployee();
+      break;
+    case "addDepartment":
+      addDepartment();
+      break;
+    case "addRole":
+      addRole();
+      break;
+    case "updateRole":
+      updateRole();
+      break;
+    case "quit":
+      quit();
+      break;
+  }
+}
+
+function viewEmployees() {
+  connection.query(
+    "SELECT id, first_name, last_name, role_id, manager_id FROM employees",
+    function(err, res) {
+      if (err) throw err;
+      console.table(res);
+      mainMenu();
+    }
+  );
+}
+
+function viewDepartments() {
+  connection.query("SELECT * FROM departments", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    mainMenu();
+  });
+}
+
+function viewRoles() {
+  connection.query("SELECT * FROM roles", function(err, res) {
+    if (err) throw err;
+    console.table(res);
+    mainMenu();
+  });
+}
+
+function quit() {
+  console.log("See you next time");
+  connection.end();
 }
